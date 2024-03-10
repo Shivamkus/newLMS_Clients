94c9f479b59273f2b4de86668e1fe00b957f9e92
Shivamkus <kushwahshivam065@.com>
2024-03-10 16:14:43 +0530

    Update file:

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com','randomuser.me'],
-      },
-      experimental:{
-        reactRoot: true,
-        suppressHydrationWarning: true,
-      }
+  images: {
+      domains: ['res.cloudinary.com','randomuser.me'],
+    },
+    experimental:{
+      reactRoot: true,
+      suppressHydrationWarning: true,
+    }
 }
-// module.exports = nextConfig
-
 export default nextConfig;
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
 
-// export default nextConfig;
